7efe1462a5df43573706d91735e22a2ce5a0409d
imber <a1157911285@163.com>
2022-06-20 18:32:48 +0800

    feat：虚拟列表

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,34 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container">
+    <virtual-recycle-scroller :items="list" />
   </div>
 </template>
+
+<script>
+import virtualRecycleScroller from './virtual.vue'
+// 模拟一个长列表
+const list = []
+for (let i = 0; i < 50; i++) {
+  list.push({
+    id: i,
+    label: `virtual-list ${i}`
+  })
+}
+export default {
+  components: {
+    virtualRecycleScroller
+  },
+  data() {
+    return {
+      list: list
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  height: 600px;
+  border: 1px solid #ccc;
+}
+</style>
